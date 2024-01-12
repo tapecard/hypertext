b8a1b8b23139611dcaf8d60d2050f1f1caf65f53
Mike Cohen <mike.cohen@tapecard.com>
2024-01-12 16:05:34 -0500

    Speed Style Update
    converts to em for scalability
    cleans up css

--- src/components/SpeedStyle.vue
+++ src/components/SpeedStyle.vue
@@ -20,72 +20,34 @@ export default {
 </script>
 
 <style>
-.display.speed {
+.speed {
   text-align: center;
   overflow: visible;
-  text-align: center;
-  transform: skewX(-22deg);
-  text-shadow: 
-    30px 0 0 rgba(0, 0, 0, .25), 
-    -50px 0 0 rgba(0, 0, 0, .25), 
-    50px 0 0 rgba(0, 0, 0, .35);
   animation: speeds 24ms infinite;
-}
-.speed:before {
-  content: '';
-  transform: skewX(-22deg);
-  border-top: 2px dashed rgba(255, 255, 255, 1);
-  border-bottom: 2px dashed rgba(255, 255, 255, 1);
-  width: 100%;
-  height: 5px;
-  left: -50%;
-  position: absolute;
-  transform: translate(30px, 18px);
-  animation: speedbefore 150ms infinite;
-  z-index: 3;
 }
 @keyframes speeds {
   0% {
+    transform: skewX(-22deg);
     text-shadow: 
-      -2px 0 0 rgba(0, 0, 0, .3), 
-      -5px 0 0 rgba(0, 0, 0, .2),
-      -10px 0 0 rgba(0, 0, 0, .1),
-      -18px 0 0 rgba(0, 0, 0, .075);
+      -.38em 0 0 rgba(0, 0, 0, .3), 
+      -.16em 0 0 rgba(0, 0, 0, .2),
+      -.35em 0 0 rgba(0, 0, 0, .1),
+      -.75em 0 0 rgba(0, 0, 0, .075);
     }
     100% {
+      transform: skewX(-24deg);
     text-shadow: 
-      -2px 0 0 rgba(0, 0, 0, .1), 
-      -5px 0 0 rgba(0, 0, 0, .2),
-      -10px 0 0 rgba(0, 0, 0, .1),
-      -15px 0 0 rgba(0, 0, 0, .075),
+      -.41em 0 0 rgba(0, 0, 0, .01), 
+      -.21em 0 0 rgba(0, 0, 0, .02),
+      -.06em 0 0 rgba(0, 0, 0, .4),
+      -0.8em 0 0 rgba(0, 0, 0, .075),
 
-      -190px 0 0 rgba(0, 0, 0, .15),
-      -50px 0 0 rgba(0, 0, 0, .15), 
-      10px 0 0 rgba(0, 0, 0, .1), 
-      5px 0 0 rgba(0, 0, 0, .1), 
-      -100px 0 0 rgba(0, 0, 0, .25),
-      -120px 0 0 rgba(0, 0, 0, .25);
-    }
-}
-@keyframes speedbefore {
-  0% {
-    }
-    20% {
-      transform: translate(15px, 1px);
-    }21% {
-      transform: translate(-15px, 5px);
-    }
-    50% {
-      transform: translate(-15px, 4px);
-    }
-    51% {
-      transform: translate(0, -1px);
-    }
-    70% {
-      transform: translate(-2px, 5px);
-    }
-    100% {
-      transform: translate(-30px, 2px);
+      -1.6em 0 0 rgba(0, 0, 0, .15),
+      -2em 0 0 rgba(0, 0, 0, .05), 
+      .43em 0 0 rgba(0, 0, 0, .1), 
+      .11em 0 0 rgba(0, 0, 0, .15), 
+      -4em 0 0 rgba(0, 0, 0, .15),
+      -5em 0 0 rgba(0, 0, 0, .15);
     }
 }
 </style>
